10ec5e822e6ffc010e82181b53fd96d86dd2caf2
Piyusha <pxsha23@gmail.com>
2026-05-23 13:30:03 +0530

    Update compare.app.js

--- compare.app.js
+++ compare.app.js
@@ -1,5 +1,5 @@
 // --- Global variables ---
-const API_URL = 'http://127.0.0.1:5000';
+const API_URL = 'https://greengrid-cj3v.onrender.com';
 let wardsData = [];
 
 // --- Main function: Runs when the page loads ---
